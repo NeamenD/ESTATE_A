cb9ca32b21854a09601a7abffd400b757f00481f
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-05-27 17:56:40 -0600

    added a search image logo and style the buy and rent button

--- frontend/src/components/searchBar/SearchBar.jsx
+++ frontend/src/components/searchBar/SearchBar.jsx
@@ -1,5 +1,54 @@
+import { useState } from "react";
 import "./searchBar.scss";
 
+const types = ["buy", "rent"];
+
 export default function SearchBar() {
-  return <div>SearchBar</div>;
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+  const switchType = (val) => {
+    setQuery((prev) => ({ ...prev, type: val }));
+  };
+
+  return (
+    <div className="searchBar">
+      <div className="type">
+        {types.map((type) => (
+          <button
+            key={type}
+            onClick={() => switchType(type)}
+            className={query.type === type ? "active" : ""}
+          >
+            {type}
+          </button>
+        ))}
+      </div>
+      <div className="type"></div>
+      <form action="text">
+        <input type="text" name="location" placeholder="City Location" />
+        <input
+          type="text"
+          name="minPrice"
+          min={0}
+          max={1000000}
+          placeholder="Min Price"
+        />
+        <input
+          type="text"
+          name="maxPrice"
+          min={0}
+          max={1000000}
+          placeholder="Max Price"
+        />
+        <button>
+          <img src="img/search.png" alt="search logo" />
+        </button>
+      </form>
+    </div>
+  );
 }
