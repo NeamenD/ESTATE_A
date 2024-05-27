05c27813d203f9ddac5eb923d1be49552078d026
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-05-27 12:35:47 -0600

    style homePage

--- frontend/src/routes/homePage/homePage.jsx
+++ frontend/src/routes/homePage/homePage.jsx
@@ -1,18 +1,39 @@
+import SearchBar from "../../components/searchBar/SearchBar";
 import "./homePage.scss";
 
 export default function HomePage() {
   return (
     <div className="homePage">
       <div className="textContainer">
-        <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
-        <p>
-          Discover the perfect home with our extensive real estate listings.
-          Whether you're looking for a cozy apartment in the city or a spacious
-          house in the suburbs, we have options to suit every preference and
-          budget. Our experienced agents are here to help you navigate the
-          market and find the property that fits your lifestyle. Start your
-          journey to owning your dream place today!
-        </p>
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Discover the perfect home with our extensive real estate listings.
+            Whether you're looking for a cozy apartment in the city or a
+            spacious house in the suburbs, we have options to suit every
+            preference and budget. Our experienced agents are here to help you
+            navigate the market and find the property that fits your lifestyle.
+            Start your journey to owning your dream place today!
+          </p>
+          <SearchBar />
+
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of experience</h2>
+            </div>
+
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="imgContainer">
         <img src="img/bg.png" alt="Background image of building" />
