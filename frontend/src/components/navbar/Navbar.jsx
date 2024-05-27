c318a3ce836d6f2af48e6dae1aadf5974365842d
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-05-27 09:32:17 -0600

    installed scss dependence and adde logo

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,8 +1,22 @@
+import "./navbar.scss";
+
 export default function Navbar() {
   return (
     <nav>
-      <div className="left">Left</div>
-      <div className="right">Right</div>
+      <div className="left">
+        <a href="/">
+          <img src="../../../public/img/GOJO-removebg-preview.png" alt="Logo" />
+          <span>GOJO</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/">Sign in</a>
+        <a href="/">Sign up</a>
+      </div>
     </nav>
   );
 }
