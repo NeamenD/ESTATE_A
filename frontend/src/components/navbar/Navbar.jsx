290e576e030bf6e6661500a4da83820a68bf400b
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-05-27 10:17:06 -0600

    add a nav bar style and a menu bar

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -4,9 +4,9 @@ export default function Navbar() {
   return (
     <nav>
       <div className="left">
-        <a href="/">
+        <a href="/" className="logo">
           <img src="../../../public/img/GOJO-removebg-preview.png" alt="Logo" />
-          <span>GOJO</span>
+          {/* <span>GOJO</span> */}
         </a>
         <a href="/">Home</a>
         <a href="/">About</a>
@@ -15,7 +15,21 @@ export default function Navbar() {
       </div>
       <div className="right">
         <a href="/">Sign in</a>
-        <a href="/">Sign up</a>
+        <a href="/" className="register">
+          Sign up
+        </a>
+        <div className="menuIcon">
+          <img src="../../../public/img/menu.png" alt="Menu bar icons" />
+        </div>
+
+        <div className="menu">
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
       </div>
     </nav>
   );
