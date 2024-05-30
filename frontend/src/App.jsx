cbf848f0c320e0fb74ec3c66fc3ee93c61cebc85
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-05-30 10:11:53 -0600

    added a a folder templet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,29 @@
-import "./layout.scss";
 import HomePage from "./routes/homePage/homePage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Listpage from "./routes/listPage/Listpage";
+import Layout from "./routes/layout/layout";
+import SinglePage from "./routes/singlePage/singlePage.jsx";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <HomePage />,
-    },
-    {
-      path: "/list",
-      element: <Listpage />,
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <Listpage />,
+        },
+
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+      ],
     },
   ]);
   return (
