d0e28ab74e940c325d4b1bd9037b459f4cbce98c
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-05-30 11:24:57 -0600

    filter component search catagory

--- frontend/src/routes/listPage/Listpage.jsx
+++ frontend/src/routes/listPage/Listpage.jsx
@@ -1,11 +1,20 @@
 import { listData } from "../../components/lib/dummydata";
 import "./listpage.scss";
+import Filter from "../../components/filter/Filter";
+import Card from "../../components/card/Card";
 
 export default function Listpage() {
   const data = listData;
   return (
     <div className="listPage">
-      <div className="listContainer">List</div>
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
       <div className="mapContainer">Map</div>
     </div>
   );
