b2871b5a0eefe67eb29b6997ac5a19136daa3bab
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-05-30 09:33:49 -0600

    list page lay out using react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,31 @@
-import Navbar from "./components/navbar/Navbar";
 import "./layout.scss";
 import HomePage from "./routes/homePage/homePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Listpage from "./routes/listPage/Listpage";
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+    {
+      path: "/list",
+      element: <Listpage />,
+    },
+  ]);
   return (
     <>
-      <div className="layout">
+      {/* <div className="layout">
         <div className="navbar">
           <Navbar />
         </div>
         <div className="content">
           <HomePage />
         </div>
-      </div>
+      </div> */}
+
+      <RouterProvider router={router} />
     </>
   );
 }
---
+++ frontend/src/routes/listPage/Listpage.jsx
@@ -0,0 +1,5 @@
+import "./listpage.scss";
+
+export default function Listpage() {
+  return <div className="listPage">Listpage</div>;
+}
