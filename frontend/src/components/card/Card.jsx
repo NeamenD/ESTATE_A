a25b36d085b8dab7c0c5c93cb387681f4ffc4dc1
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-05-30 14:51:58 -0600

    created a card component for each item added image

--- frontend/src/components/card/Card.jsx
+++ frontend/src/components/card/Card.jsx
@@ -1,5 +1,47 @@
-function Card() {
-  return <div>Card</div>;
+import { Link } from "react-router-dom";
+import "./card.scss";
+
+function Card({ item }) {
+  return (
+    <div className="card">
+      <Link to={`/${item.id}`} className="imageContainer">
+        <img src={item.img} alt="" />
+      </Link>
+
+      <div className="textContainer">
+        <h2 className="title">
+          <Link to={`/${item.id}`}>{item.title}</Link>
+        </h2>
+        <p className="address">
+          <img src="./img/pin.png" alt="" />
+          <span>{item.address}</span>
+        </p>
+        <p className="price">$ {item.price}</p>
+
+        <div className="bottom">
+          <div className="features">
+            <div className="feature">
+              <img src="./img/bed.png" alt="image logo" />
+              <span>{item.bedroom} bedroom</span>
+            </div>
+
+            <div className="feature">
+              <img src="./img/bath.png" alt="image logo" />
+              <span>{item.bedroom} bedroom</span>
+            </div>
+          </div>
+          <div className="icons">
+            <div className="icon">
+              <img src="../../../public/img/save.png" alt="save icon" />
+            </div>
+            <div className="icon">
+              <img src="../../../public/img/chat.png" alt="chat icon" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
