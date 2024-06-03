b799b395e0b8539ea513738abbdb40edfb2fa3f4
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-06-03 16:39:44 -0600

    style the chat Messages

--- frontend/src/components/chat/chat.jsx
+++ frontend/src/components/chat/chat.jsx
@@ -49,9 +49,43 @@ function chat() {
         </div>
       </div>
       <div className="chatBox">
-        <div className="top"></div>
-        <div className="center"></div>
-        <div className="bottom"></div>
+        <div className="top">
+          <div className="user">
+            <img
+              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+            />
+            John Doe
+          </div>
+          <span className="close">X</span>
+        </div>
+        <div className="center">
+          <div className="chatMessage own">
+            <p>Lorem ipsum dolor sit amet consectetur</p>
+            <span>1 hour ago</span>
+          </div>
+
+          <div className="chatMessage">
+            <p>Lorem ipsum dolor sit amet consectetur</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="chatMessage ">
+            <p>Lorem ipsum dolor sit amet consectetur</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="chatMessage own">
+            <p>Lorem ipsum dolor sit amet consectetur</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="chatMessage">
+            <p>Lorem ipsum dolor sit amet consectetur</p>
+            <span>1 hour ago</span>
+          </div>
+        </div>
+        <div className="bottom">
+          <textarea name="" id=""></textarea>
+          <button>Send</button>
+        </div>
       </div>
     </div>
   );
