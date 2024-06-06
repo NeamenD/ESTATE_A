7ebd6dc5d30b2e03f9e14e7eb77e07efcaca495a
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-06-06 10:21:50 -0600

    login check if the passwoed is correct

--- backend/api/controllers/auth.controller.js
+++ backend/api/controllers/auth.controller.js
@@ -26,8 +26,28 @@ export const register = async (req, res) => {
   //db operations
   //   console.log(req.body);
 };
-export const login = (req, res) => {
-  //db operations
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    //check if the user exists
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+    if (!user) return res.status(404).json({ message: "invalid credentials!" });
+    //check if the password is correct
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid)
+      return res.status(401).json({ message: "Invalid Credentials!" });
+
+    res.serHeader("Set-Cookie", "test" + "myValue");
+    //generate cookie token and send to user
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to login!" });
+  }
 };
 export const logout = (req, res) => {
   //db operations
