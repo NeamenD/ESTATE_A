8c24e782acf302fd20c9f57ba0042a20cc3ad238
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-06-06 09:35:40 -0600

    unique user login

--- backend/api/controllers/auth.controller.js
+++ backend/api/controllers/auth.controller.js
@@ -1,16 +1,31 @@
 import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
+  try {
+    //hash password
 
-  //hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // console.log(hashedPassword);
+    //create a new user and save to db
 
-  const hashedPassword = await bcrypt.hash(password, 10);
-  console.log(hashedPassword);
-  //create a new user and save to db
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+    console.log(newUser);
+    res.status(200).json({ message: "User crated successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to create user!" });
+  }
+
   //db operations
   //   console.log(req.body);
 };
-
 export const login = (req, res) => {
   //db operations
 };
