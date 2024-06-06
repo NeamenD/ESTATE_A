88748e82ec6cbbb9c39919476dad297be4e69600
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-06-06 16:17:55 -0600

    added a login page form

--- frontend/src/routes/login/login.jsx
+++ frontend/src/routes/login/login.jsx
@@ -1,10 +1,11 @@
 import { useContext, useState } from "react";
 import "./login.scss";
+import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const { updateUser } = useContext(AuthContext);
+  // const { updateUser } = useContext(AuthContext);
 
   const navigate = useNavigate();
 
@@ -33,7 +34,37 @@ function Login() {
     }
   };
 
-  return <div className="login">login</div>;
+  return (
+    <div className="login">
+      <div className="formContainer">
+        <form onSubmit={handleSubmit}>
+          <h1>Welcome back</h1>
+          <input
+            name="username"
+            required
+            minLength={3}
+            maxLength={20}
+            type="text"
+            placeholder="Username"
+          />
+          <input
+            name="password"
+            type="password"
+            required
+            placeholder="Password"
+          />
+          <button type="submit" disabled={isLoading}>
+            {isLoading ? "Logging in..." : "Login"}
+          </button>
+          {error && <span className="error">{error}</span>}
+          <Link to="/register">{"Don't"} you have an account?</Link>
+        </form>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="Background" />
+      </div>
+    </div>
+  );
 }
 
 export default Login;
