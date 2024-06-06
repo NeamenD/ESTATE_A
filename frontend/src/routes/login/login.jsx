5c611d4fa53a6f91b731c77d9aead15b24e3d185
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-06-06 16:00:30 -0600

    login error handling

--- frontend/src/routes/login/login.jsx
+++ frontend/src/routes/login/login.jsx
@@ -1,6 +1,38 @@
+import { useContext, useState } from "react";
 import "./login.scss";
 
 function Login() {
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const { updateUser } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError("");
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const password = formData.get("password");
+
+    try {
+      const res = await apiRequest.post("/auth/login", {
+        username,
+        password,
+      });
+
+      updateUser(res.data);
+
+      navigate("/");
+    } catch (err) {
+      setError(err.response?.data?.message || "Login failed");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return <div className="login">login</div>;
 }
 
