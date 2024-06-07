d644ca3bb1b33517fcf84b4b27f4483dfb8dd9f0
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-06-07 13:50:06 -0600

    middleware

--- backend/api/controllers/test.controller.js
+++ backend/api/controllers/test.controller.js
@@ -3,22 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const shouldBeLoggedIn = async (req, res, next) => {
-  const token = req.cookies.token;
-
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
-  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-    if (err) {
-      return res.status(403).json({ message: "Token is not valid!" });
-    } else {
-      // Token is valid, set user information in the request
-      res.status(200).json({ message: "you are authenticated" });
-      //   req.user = payload;
-      //   next();
-    }
-  });
+  console.log(req.userId);
+  res.status(200).json({ message: "you are authenticated" });
 };
 export const shouldBeAdmin = async (req, res) => {
   const token = req.cookies.token;
