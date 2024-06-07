24794e1abd42b8f3155e7cd1d1231a18373d5c6d
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-06-07 09:20:51 -0600

    login routh and auth

--- backend/api/app.js
+++ backend/api/app.js
@@ -2,8 +2,10 @@ import express from "express";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 
 app.use(cookieParser());
