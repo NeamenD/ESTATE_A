326923b57209731925f0d701d6c7992fb8d239d1
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-06-10 13:14:14 -0600

    update userer profile

--- backend/api/controllers/user.controller.js
+++ backend/api/controllers/user.controller.js
@@ -1,25 +1,45 @@
+import prisma from "../lib/prisma.js";
+
 export const getUsers = async (req, res) => {
   //   console.log("it works");
   try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
   } catch (error) {
     console.log(error);
-    es.status(500).json({ message: "Failed to get users!" });
+    res.status(500).json({ message: "Failed to get users!" });
   }
 };
 
 export const getUser = async (req, res) => {
+  const id = req.params.id;
   try {
+    const user = await prisma.user.findUnique({ where: { id } }); //only one
+    res.status(200).json(user);
   } catch (error) {
     console.log(error);
-    es.status(500).json({ message: "Failed to get user!" });
+    res.status(500).json({ message: "Failed to get user!" });
   }
 };
 
 export const updateUser = async (req, res) => {
+  const id = req.params.id;
+
+  const tokenUserId = req.userId;
+
+  const body = req.body;
+
+  if (id !== tokenUserId) {
+    return res.status(403).json({ message: "Not Authorized" });
+  }
   try {
+    const updatedUser = await prisma.user.update({
+      where: { id },
+      data: body,
+    });
   } catch (error) {
     console.log(error);
-    es.status(500).json({ message: "Failed to update users!" });
+    res.status(500).json({ message: "Failed to update users!" });
   }
 };
 
@@ -27,6 +47,6 @@ export const deleteUser = async (req, res) => {
   try {
   } catch (error) {
     console.log(error);
-    es.status(500).json({ message: "Failed to delete users!" });
+    res.status(500).json({ message: "Failed to delete users!" });
   }
 };
