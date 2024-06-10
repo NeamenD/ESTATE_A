9682a1bf2a0011f7ba45a05013a30eb9e74e63bc
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-06-09 23:59:08 -0600

    crud update schema

--- backend/api/controllers/post.controller.js
+++ backend/api/controllers/post.controller.js
@@ -2,31 +2,51 @@ import prisma from "../lib/prisma.js";
 export const getPosts = async (req, res) => {
   try {
     const posts = await prisma.post.findMany();
-  } catch (error) {
     res.status(200).json(posts);
+  } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Failed to get posts" });
   }
 };
 
 export const getPost = async (req, res) => {
-  const id = req.params;
+  const id = req.params.id;
   try {
     const post = await prisma.post.findUnique({
       where: { id },
+      include: {
+        postDetail: true,
+        user: {
+          select: {
+            username: true,
+            avatar: true,
+          },
+        },
+      },
     });
-  } catch (error) {
+
     res.status(200).json(post);
+  } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Failed to get post" });
   }
 };
 
-export const addPosts = async (req, res) => {
+export const addPost = async (req, res) => {
   const body = req.body;
   const tokenUserId = req.userId;
 
   try {
+    const newPost = await prisma.post.create({
+      data: {
+        ...body.postData,
+        userId: tokenUserId,
+        postDetail: {
+          create: body.postDetail,
+        },
+      },
+    });
+    res.status(200).json(newPost);
   } catch (error) {
     res.status(200).json();
     console.log(error);
@@ -44,7 +64,18 @@ export const updatePost = async (req, res) => {
 };
 
 export const deletePost = async (req, res) => {
+  const id = req.params.id;
+  const tokenUserId = req.userId;
   try {
+    const post = await prisma.post.findUnique({
+      where: { id },
+    });
+    if (post.userId !== tokenUserId) {
+      return res.status(403).json({ message: "Not Authorized" });
+    }
+
+    await prisma.post.delete({ where: { id } });
+    res.status(200).json({ message: "post deleted" });
   } catch (error) {
     res.status(200).json();
     console.log(error);
--- backend/api/routes/post.route.js
+++ backend/api/routes/post.route.js
@@ -1,17 +1,18 @@
 import express from "express";
 import { verifyToken } from "../middleware/verifyToken.js";
 import {
-  addPosts,
+  addPost,
   deletePost,
   getPost,
   getPosts,
   updatePost,
 } from "../controllers/post.controller.js";
+
 const router = express.Router();
 
 router.get("/", getPosts);
 router.get("/:id", getPost);
-router.post("/", verifyToken, addPosts);
+router.post("/", verifyToken, addPost);
 router.put("/:id", verifyToken, updatePost);
 router.delete("/:id", verifyToken, deletePost);
 
