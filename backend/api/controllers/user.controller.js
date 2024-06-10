aaf30a973e30491bebda9cfa82ef5cd144af97ba
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-06-10 12:44:56 -0600

    endpoint works

--- backend/api/controllers/user.controller.js
+++ backend/api/controllers/user.controller.js
@@ -1,7 +1,32 @@
 export const getUsers = async (req, res) => {
+  //   console.log("it works");
   try {
   } catch (error) {
     console.log(error);
-    res.s;
+    es.status(500).json({ message: "Failed to get users!" });
+  }
+};
+
+export const getUser = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    es.status(500).json({ message: "Failed to get user!" });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    es.status(500).json({ message: "Failed to update users!" });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    es.status(500).json({ message: "Failed to delete users!" });
   }
 };
