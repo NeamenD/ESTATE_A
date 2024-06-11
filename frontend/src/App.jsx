15ae7d0d2e0b581b08736b060ba4fdb80302716b
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-06-11 13:09:39 -0600

    use loader data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import Register from "./routes/register/register.jsx";
 import Login from "./routes/login/login.jsx";
 import ProfileUpdatePage from "./routes/profileUpdatePage/profileUpdatePage.jsx";
 import NewPostPage from "./routes/newPostPage/newPostPage.jsx";
-import { singlePageLoader } from "./lib/loaders.js";
+import { listPageLoader, singlePageLoader } from "./lib/loaders.js";
 
 function App() {
   const router = createBrowserRouter([
@@ -23,6 +23,7 @@ function App() {
         {
           path: "/list",
           element: <Listpage />,
+          loader: listPageLoader,
         },
 
         {
