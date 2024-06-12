1b1354e22e7fdf3eb0086863b88d73b8623f033f
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-06-12 15:32:50 -0600

    added a auto swiper scroll image

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,6 +1,20 @@
-// About.js
-import React from "react";
-import "./about.scss"; // Create this file for About-specific styles if needed
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "./about.scss";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
+// Import your image files (adjust paths as necessary)
+import image1 from "/RealEstate/image1.jpg";
+import image2 from "/RealEstate/image2.jpg";
+import image3 from "/RealEstate/image3.jpg";
+import image4 from "/RealEstate/image4.jpg";
+import image5 from "/RealEstate/image5.jpg";
+import image6 from "/RealEstate/image6.jpg";
+import image7 from "/RealEstate/image7.jpg";
+import image8 from "/RealEstate/image8.jpg";
+import image9 from "/RealEstate/image9.jpg";
 
 const About = () => {
   return (
@@ -25,6 +39,48 @@ const About = () => {
           agents, and embark on your journey to finding your dream home in
           Denver today!
         </p>
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img src={image1} alt="Property 1" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image2} alt="Property 2" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image3} alt="Property 3" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image4} alt="Property 4" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image5} alt="Property 5" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image6} alt="Property 5" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image7} alt="Property 5" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image8} alt="Property 5" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image9} alt="Property 5" />
+          </SwiperSlide>
+        </Swiper>
       </div>
     </div>
   );
--- frontend/src/routes/Agents/Agents.jsx
+++ frontend/src/routes/Agents/Agents.jsx
@@ -1,5 +1,4 @@
 // src/pages/Agents.js
-import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
